ebc85af43aeea0c842ed625704e0315dbc0659a6
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-22 15:19:15 -0400

    add title and subtitle

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Am I Otter Not?!?
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          {"mustelids don't give a shit"}
         </p>
 
         <div className={styles.grid}>
