b29103d0904851fa40a8ea5a0e6ba399fce16de6
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-29 12:49:53 -0400

    add fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import '@fontsource/roboto/500.css';
+import '@fontsource/archivo-black/400.css';
 import "../styles/globals.css";
 
 const colors = {
@@ -9,7 +11,14 @@ const colors = {
   },
 };
 
-const theme = extendTheme({ colors });
+const fonts = {
+  fonts: {
+    main: 'Archivo Black, sans-serif',
+    secondary: 'Roboto, sans-serif'
+  },
+};
+
+const theme = extendTheme({ colors, fonts });
 
 function OtterNot({ Component, pageProps }) {
   return (
