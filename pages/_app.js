782d50884a9f937d7b97cba3d71dae13f9778fcd
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-29 20:44:46 -0400

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/archivo-black/400.css';
 import "../styles/globals.css";
 
-// const fonts = {
-//   fonts: {
-//     main: 'Archivo Black, sans-serif',
-//     secondary: 'Roboto, sans-serif'
-//   },
-// };
-
-// const theme = extendTheme({ colors, fonts });
-
 function OtterNot({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
