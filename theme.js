4b92bd94979d08325670f48bb7869fff3e0e316f
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-30 00:43:35 -0400

    colors are part of default theme, add fonts

--- theme.js
+++ theme.js
@@ -1,30 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export default extendTheme({
-  colors: {
-    yellow: {
-      50: '#fffff0',
-      100: '#fefcbf',
-      200: '#faf089',
-      300: '#f6e05e',
-      400: '#ecc94b',
-      500: '#d69e2e',
-      600: '#b7791f',
-      700: '#975a16',
-      800: '#744210',
-      900: '#5f370e',
-    },
-    green: {
-      50: '#F0FFF4',
-      100: '#C6F6D5',
-      200: '#9AE6B4',
-      300: '#68D391',
-      400: '#48BB78',
-      500: '#38A169',
-      600: '#2F855A',
-      700: '#276749',
-      800: '#22543D',
-      900: '#1C4532',
-    },
+  fonts: {
+    primary: 'Archivo Black, sans-serif',
+    secondary: 'Roboto, sans-serif'
   }
 });
