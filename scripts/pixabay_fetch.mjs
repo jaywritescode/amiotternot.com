c6a56e66b792f54add68cbc313abe27c6c39b0bd
Jay Harris <jaywritescode@users.noreply.github.com>
2022-05-01 19:23:56 -0400

    script to request an image from pixabay and save it on disk

---
+++ scripts/pixabay_fetch.mjs
@@ -0,0 +1,54 @@
+import { createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import sqlite3 from 'sqlite3';
+
+const streamPipeline = promisify(pipeline);
+
+const db = new sqlite3.Database(':memory:');
+
+const loadEnv = dotenv.config({path: '.env.local'});
+if (loadEnv.error) {
+  throw loadEnv.error;
+}
+
+async function main(keyword) {
+  const response = await fetch(
+    `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${keyword}&image_type=photo`
+  );
+
+  const data = await response.json();
+  const { hits } = data;
+
+  const {
+    id,
+    webformatWidth,
+    webformatHeight,
+    user,
+    user_id,
+    webformatURL
+  } = hits[0];
+  const image = await fetch(webformatURL);
+  
+  await streamPipeline(image.body, createWriteStream('mustelid.jpg'));
+  
+
+  // const blob = await image.blob();
+  
+  // db.serialize(() => {
+  //   db.run("CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY, original_id, source, width, height, notes, data");
+
+  //   db.run("INSERT INTO images VALUES (?, ?, ?, ?, ?, ?)", 
+  //     [id, webformatURL, webformatWidth, webformatHeight, user, blob]);
+
+
+  //   db.each("SELECT rowid AS id, original_id, source, width, height, notes, data FROM images", (err, row) => {
+  //     console.log(row.id + ": " + row.info);
+  //   })
+  // });
+}
+
+main('ferret');
