f43e00adadd57e5ee90c289f7b775b303840ba67
Jay Harris <jaywritescode@users.noreply.github.com>
2022-05-06 21:03:29 -0400

    scale this damn svg

--- components/Attribution.jsx
+++ components/Attribution.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import pixabay from "../public/pixabay.svg"
 
 export default function Attribution({ user, user_id }) {
   return (
     <>
       <a href={`https://pixabay.com/users/${user}-${user_id}`}>
         {"image by "}
-        <Image src="/pixabay.svg" alt="pixabay" height={16} width={30} />
+        <Image src={pixabay} alt="pixabay" layout="intrinsic" height={16} width={82} />
         {` user ${user}`}
       </a>
     </>
