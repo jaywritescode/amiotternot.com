60c4fab861f8195a7d4129189fc96ccf68d372e7
Jay Harris <jaywritescode@users.noreply.github.com>
2022-05-18 22:33:40 -0400

    fix giant creative commons icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css"
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import theme from "../theme";
 import "@fontsource/roboto/500.css";
 import "@fontsource/archivo-black/400.css";
 import "../styles/globals.css";
 
-config.autoAddCss = false
+config.autoAddCss = false;
 
 function OtterNot({ Component, pageProps }) {
   return (
