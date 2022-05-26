e645a1b9ca11b1d582ee44bd315f639ef963ba62
Jay Harris <jaywritescode@users.noreply.github.com>
2022-05-25 23:15:47 -0400

    update to postgresql in vote.js

--- pages/api/[image_id]/vote.js
+++ pages/api/[image_id]/vote.js
@@ -1,21 +1,25 @@
-import sqlite3 from "sqlite3";
+import { Client } from "pg";
 
-const db = new sqlite3.Database("pics.db");
-sqlite3.verbose();
+const client = new Client({
+  connectionString: process.env.DATABASE_URL + "?sslmode=disable",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const {
     query: { image_id },
   } = req;
   const body = JSON.parse(req.body);
 
-  db.serialize(() => {
-    db.run(
-      "INSERT INTO votes (image_id, is_otter) VALUES (?, ?)",
-      image_id,
-      body.isOtter
-    );
-  });
+  await client.connect();
+
+  client.query(
+    "INSERT INTO amiotternot.votes (image_id, is_otter) VALUES (?, ?)",
+    image_id,
+    body.isOtter
+  );
 
   res.send(204);
 }
