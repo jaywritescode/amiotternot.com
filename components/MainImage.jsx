2f294b1c01f36188c69446f4d78e9bc6c3b42f61
Jay Harris <jaywritescode@users.noreply.github.com>
2022-06-04 17:21:43 -0400

    scale main image correctly

--- components/MainImage.jsx
+++ components/MainImage.jsx
@@ -4,12 +4,24 @@ import otter from "../public/otter.png";
 export default function MainImage(props) {
   const { image_id, width, height, keyword } = props;
 
+  const is_landscape = width >= height;
+
+  let rendered_width, rendered_height;
+  if (is_landscape) {
+    rendered_width = 640;
+    rendered_height = height * 640 / width;
+  }
+  else {
+    rendered_height = 480;
+    rendered_width = width * 480 / height;
+  }
+
   if (image_id) {
     return (
       <Image
         src={`/pics/${image_id}.jpg`}
-        width={width}
-        height={height}
+        width={rendered_width}
+        height={rendered_height}
         alt={`${keyword} pic`}
       />
     );
